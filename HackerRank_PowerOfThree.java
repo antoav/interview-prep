42c2fbbcb5d06d83eb6bbf2a9c71f50b27aa4055
antoav <anto.avog@gmail.com>
2020-06-27 10:49:24 -0400

    Fixed branching error

--- HackerRank_PowerOfThree.java
+++ HackerRank_PowerOfThree.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 POWER OF 3
 given an integer, write a function to determine
@@ -14,20 +13,3 @@ public static boolean isPowerOfThree(int number){
     else
         return false;
 }
-=======
-/*
-POWER OF 3
-given an integer, write a function to determine
-if its a power of 3. 
-Return 1 if yes, 0 if no.
-*/
-
-public static boolean isPowerOfThree(int number){
-    if(number == 1)
-        return true;
-    if(number > 0 && 1162261467 % number == 0)
-        return true;
-    else
-        return false;
-}
->>>>>>> 8149bd96b4aa204faec55bd07b3abed6d09576ad
