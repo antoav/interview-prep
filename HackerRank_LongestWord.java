1735e8292279bb056a56d80cba1f6fb64cee54d8
antoav <anto.avog@gmail.com>
2020-06-27 15:24:06 -0400

    uploading unsolved problem

---
+++ HackerRank_LongestWord.java
@@ -0,0 +1,15 @@
+/*
+    LONGEST WORD
+
+    given an array of strings, return the longest word. 
+    
+    eg. 
+    given: ["hello elephants", "hi bugs", "yo snakes"]
+    return elephants
+*/   
+ 
+
+
+    public static String longestWord(List<String> words) {
+
+    }
